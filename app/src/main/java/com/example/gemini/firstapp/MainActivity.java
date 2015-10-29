0ab2d4145ffa4fa09e43cb97ef052767af2ddf5c
Tom Pelsmaeker <tlpelsmaeker@gmail.com>
2015-10-29 16:13:01 +0100

    Building a number guessing game

--- app/src/main/java/com/example/gemini/firstapp/MainActivity.java
+++ app/src/main/java/com/example/gemini/firstapp/MainActivity.java
@@ -8,6 +8,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +21,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // generate a random number
+        Random generator = new Random();
+        int randomNumber = generator.nextInt(100);
+
+        final TextView firstTextView = (TextView) findViewById(R.id.textView);
+        Button firstButton = (Button) findViewById(R.id.firstButton);
+        EditText
+
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText) findViewById(R.id.editText);
+                int guessNumber = Integer.parseInt(input.getText().toString());
+                if (guessNumber == randomNumber) {
+                    firstTextView.setText("You Clicked");
+                }
+
+
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
